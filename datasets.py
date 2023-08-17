16d85a302e5cc74719e607e3a9d64462d987a0a4
WZY_real_ikun <zhaoyu.wang22@imperial.ac.uk>
2023-08-17 12:52:55 +0100

    debug

--- datasets.py
+++ datasets.py
@@ -442,7 +442,7 @@ class AudioSet(Dataset):
 				audio_fpath = "/vol/bitbucket/jla21/proj/data/FSD50K_lms/FSD50K.dev_audio/" + fname + ".npy"
 				lms = torch.tensor(np.load(audio_fpath)).unsqueeze(0)
 				#wav, org_sr = librosa.load(audio_fpath, sr=self.cfg.sample_rate)
-				#print("flag2")
+				print("flag2")
 				#wav = torch.tensor(wav)
 				#print("flag3")
 				#lms = (self.to_melspecgram(wav) + torch.finfo().eps).log()
